943565017f3356a63347c6115d4567211502b561
kendraoue <kendraoue@gmail.com>
2025-05-15 11:00:35 -0400

    Fixing position

--- src/components/DigitalClock.vue
+++ src/components/DigitalClock.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex bg-pink-300 rounded-xl shadow-lg px-18 py-5">
+  <div class="flex bg-pink-300 rounded-xl shadow-lg px-[4.26rem] py-5">
     <div class="w-full Truckin-font" style="text-shadow: 2px 2px 0px hotpink">
       <!-- <p class="font-bold text-white text-lg">{{ currentTime.toLocaleString() }}</p> -->
       <p class="font-bold text-white text-4xl">
