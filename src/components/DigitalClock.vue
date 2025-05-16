868f8e2092b18d82c82bd73bbd622194dca2e5df
kendraoue <kendraoue@gmail.com>
2025-05-15 20:05:17 -0400

    Trying again

--- src/components/DigitalClock.vue
+++ src/components/DigitalClock.vue
@@ -1,9 +1,9 @@
 <template>
   <div
-    class="grid grid-cols-2 bg-pink-300 rounded-xl shadow-lg w-[20rem] h-[8rem] p-4"
+    class="grid grid-cols-2 bg-pink-300 rounded-xl shadow-lg w-[20rem] h-[7rem] p-2"
   >
     <div
-      class="Truckin-font text-3xl text-white px-2"
+      class="flex items-center Truckin-font text-3xl text-white px-2"
       style="text-shadow: 2px 2px 0px hotpink"
     >
       <h2>Welcome</h2>
